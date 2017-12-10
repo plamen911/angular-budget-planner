4eab13d9478120e0fca78e7d8a04e6798b7dbe09
apple <1>
2017-12-10 21:57:27 +0200

    Logout success message

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {ToastsManager} from 'ng2-toastr';
+
 import {AuthService} from '../../core/services/authentication/auth.service';
 import {MessageService} from '../../core/services/message.service';
 
@@ -15,7 +17,8 @@ export class HeaderComponent implements OnInit {
     constructor(public authService: AuthService,
                 private route: ActivatedRoute,
                 private router: Router,
-                private messageService: MessageService) {}
+                private messageService: MessageService,
+                private toastr: ToastsManager) {}
 
     ngOnInit() {
         this.year = +this.route.snapshot.paramMap.get('year') || +(new Date()).getFullYear();
@@ -26,6 +29,7 @@ export class HeaderComponent implements OnInit {
         e.preventDefault();
         this.authService.destroySession();
         this.messageService.add('Successfully logged out.');
+        this.toastr.success('Successfully logged out.');
         this.router.navigateByUrl('/login');
     }
 }
